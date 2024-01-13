9df7b00dfdbd4425d23a2588dac5f5c02b256928
ide940 <76463377+ide940@users.noreply.github.com>
2024-01-13 23:29:45 +0800

    Update vnt-cli.sh

--- vnt-cli.sh
+++ vnt-cli.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/sh
 
 sz="$@"
 
@@ -11,11 +11,11 @@ fi
 ##vnt-cli 上无参数，退出运行
 test -z "`sysctl -a | grep 'net.ipv4.ip_forward = 1'`" && sysctl -w net.ipv4.ip_forward=1 && (logger "开启系统内核转发功能";echo "开启系统内核转发功能" ) 
 
+if [  -f "/etc/storage/started_script.sh" ] ;then
+test -n "`ps |grep 'crond'|grep '\-d10'|grep -v grep|awk '{print $1}'`" || (killall crond && /usr/sbin/crond -d10)
+fi
 
-if iptables -t nat -C POSTROUTING -j MASQUERADE &>/dev/null; then
-    echo "iptables -t nat -I POSTROUTING  -j MASQUERADE 规则存在"
-else
-   echo "iptables -t nat -I POSTROUTING  -j MASQUERADE 规则插入"
+if iptables -t nat ! -C POSTROUTING -j MASQUERADE &>/dev/null; then
 iptables -t nat -I POSTROUTING  -j MASQUERADE   
 fi
 
@@ -27,7 +27,7 @@ s=""
 else
 	ip4p=`echo "${sz}"|awk -v RS='+'  '{print $0}'|grep 's'|grep -v 'k' |awk '{print $2}'`
 ##将参数中的“+”断点进行分行，查找有“s”的行，并排除“k”的行，打印第二列
-	eval $(nslookup ${ip4p} 223.5.5.5 | awk '/2001/' |cut -d ':' -f 2-6 | awk -F: '{print "port="$3" ipa="$4" ipb="$5 }')
+	eval $(nslookup ${ip4p} 119.29.29.29 | awk '/2001/' |cut -d ':' -f 2-6 | awk -F: '{print "port="$3" ipa="$4" ipb="$5 }')
 ##查询域名，并提取出ip4p地址
 	port=$((0x$port))
 	ip1=$((0x${ipa:0:2}))
@@ -54,14 +54,62 @@ fi
 
 echo "${sz}" >> /tmp/vnt_tmp
 ##将参数记录到临时文件中
+:<<!
+test -f "/usr/bin/vnt-cli" && vnt="/usr/bin/vnt-cli"
 test -f "/etc/storage/vnt-cli" && vnt="/etc/storage/vnt-cli"
 test -f "/etc/storage/bin/vnt-cli" && vnt="/etc/storage/bin/vnt-cli"
 test -f "/etc/vnt-cli" && vnt="/etc/vnt-cli"
-test -f "/usr/bin/vnt-cli" && vnt="/usr/bin/vnt-cli"
 test -f "/tmp/vnt-cli" && vnt="/tmp/vnt-cli" 
+
 ##查找vnt-cli文件
 if [ ! -f "/etc/storage/vnt-cli" ] && [ ! -f "/etc/vnt-cli" ] && [ ! -f "/etc/storage/bin/vnt-cli" ] && [ ! -f "/tmp/vnt-cli" ] && [ ! -f "/usr/bin/vnt-cli" ] ; then
 ##上述目录都不存在vnt-cli
+!
+
+SCRIPT_DIR="$(cd $(dirname $0); pwd)"
+echo "脚本所在目录: $SCRIPT_DIR"
+
+if [ -f "${SCRIPT_DIR}/vnt_cli_set_dir.txt" ] && [ -f "$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)/vnt-cli" ] ; then
+	vnt="$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)/vnt-cli"
+	 [ ! -x "${vnt}" ] && chmod +x "${vnt}"
+if	 [ 3 -gt $(($(${vnt} -h | wc -l))) ] ;then 
+ mv "$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)/vnt-cli" "$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)/vnt0-cli" 
+vnt="" 
+fi
+# 检查是否可运行，否则重名，下一步去下载
+else
+	vnt_path="/tmp /etc /usr /etc/storage"   # 设定多个路径
+
+	for v in ${vnt_path}
+	do
+		echo "在目录$v查找"
+		vnt_cli_tmp=$(find "$v" -type f -name "vnt-cli" )
+		yes1=$?
+		if [[ ${yes1} = 0 ]] ; then # 判断是否有vnt-cli ，有为0，没有为其他数字
+			vnt_cli_psc=$(echo "${vnt_cli_tmp}" | wc -l) # 统计有多少个vnt-cli
+			echo "$v目录中有${vnt_cli_psc}个文件（vnt-cli）" 
+			for y in $(seq 1 $vnt_cli_psc)
+			do
+				vnt=$(echo "${vnt_cli_tmp}" | awk 'NR=='"$y"'{print $0}') # 第y行的vnt_cli文件
+				[ ! -x "${vnt}" ] && chmod +x "${vnt}" # 判断vnt-cli是否有执行权限
+				if [ $(($(${vnt} -h | wc -l))) -gt 3 ]; then # 判断vnt-cli是否可正常运行
+					yes=$?
+					echo "正常运行" 
+					break # 跳出y循环
+				fi
+			done
+
+			if [[ ${yes1} = 0 ]] &&  [[ ${yes} = 0 ]] ; then
+				break  # 跳出v循环
+			fi
+		fi
+	done
+
+fi
+echo "${vnt}"
+
+if [ "${vnt}" = "" ] ; then
+
 vnt="/tmp/vnt-cli" 
 
 cputype=$(uname -ms | tr ' ' '_' | tr '[A-Z]' '[a-z]')
@@ -74,8 +122,7 @@ if [ -n "$(echo $cputype | grep -E "linux.*mips.*")" ] ; then
 mipstype=$(echo -n I | hexdump -o 2>/dev/null | awk '{ print substr($2,6,1); exit}') ##通过判断大小端判断mips或mipsle
 [ "$mipstype" = "0" ] && cpucore="mips" || cpucore="mipsle"
 fi
-
-case "${cpucore}" in 
+ case "${cpucore}" in 
 	"mipsle") curl -o "${vnt}" --connect-timeout 10 --retry 3 https://gh.con.sh/https://raw.githubusercontent.com/ide940/vnt/main/vnt-cli_mipsle
 	;;
 	"mips")  curl -o "${vnt}" --connect-timeout 10 --retry 3 https://gh.con.sh/https://raw.githubusercontent.com/ide940/vnt/main/vnt-cli_mips
@@ -92,21 +139,26 @@ case "${cpucore}" in
 	;;
 esac
 ##判断CPU框架并下载对应的执行文件
-size=`du "${vnt}" |cut -d "/" -f -1 |tr -d ' '`
-test "${size}" -gt 500 || rm "${vnt}"
-##判断执行文件是否大于500k,否则删除
-if [ ! -z "`uname -a | tr [A-Z] [a-z]|grep -o wrt`" ] ;then
-size=`df -k |awk '/\/overlay$/{sub(/K$/,"",$4);print $4}'`
-test "${size}" -gt 1000 && cp "${vnt}" /etc/
-##判断系统是否为openwrt，若是并空间大于1000时就把vnt-cli文件复制到etc目录中
-fi
-if [  -d "/etc/storage" ] ;then
-size=`df -k |awk '/\/etc$/{sub(/K$/,"",$4);print $4}'|tr -d '.'|tr -d 'M'`
-test "${size}" -gt 10 && cp "${vnt}" /etc/storage
-##判断系统是否为padavan，若是并空间大于10时就把vnt-cli文件复制到storage目录中
-fi
-fi
 
+test ! -x "${vnt}" && chmod +x "${vnt}"
+ [ $(($(${vnt} -h | wc -l))) -gt 3 ] || rm -rf "${vnt}"
+##判断执行文件是否可运行,否则删除
+	if [ -f "${SCRIPT_DIR}/vnt_cli_set_dir.txt" ] && [ -d "$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)" ] ; then
+		cp "${vnt}" "$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)"
+	vnt="$(tail -n 1 ${SCRIPT_DIR}/vnt_cli_set_dir.txt)/vnt-cli"
+	else
+		if [ ! -z "`uname -a | tr [A-Z] [a-z]|grep -o wrt`" ] ;then
+			size=`df -k |awk '/\/overlay$/{sub(/K$/,"",$4);print $4}'`
+			test "${size}" -gt 1000 && cp "${vnt}" /etc/
+	##判断系统是否为openwrt，若是并空间大于1000时就把vnt-cli文件复制到etc目录中
+		fi
+		if [  -d "/etc/storage" ] ;then
+			size=`df -k |awk '/\/etc$/{sub(/K$/,"",$4);print $4}'|tr -d '.'|tr -d 'M'`
+			test "${size}" -gt 10 && cp "${vnt}" /etc/storage
+	##判断系统是否为padavan，若是并空间大于10时就把vnt-cli文件复制到storage目录中
+		fi
+	fi
+fi
 test ! -z "`uname -a | tr [A-Z] [a-z]|grep -o wrt`" &&  test -z "`opkg list-installed|grep kmod-tun`" && opkg update && opkg install kmod-tun
 ##判断openwrt有无安装tun模块，无就进行安装
 test ! -x "${vnt}" && chmod +x "${vnt}"
@@ -128,13 +180,16 @@ d="${d1}"
 fi
 ##判断参数上有无-d,无则用ip最后数字
 
+
 [  -n "`pidof vnt-cli`" ] && "${vnt}" --stop && killall vnt-cli
 ##先退出旧运行参数的进程
 sleep 2
 
-"${vnt}" $@ --no-proxy ${d} ${n} ${s} 2>&1 &
+"${vnt}" $@ --no-proxy ${d} ${n} ${s} >/tmp/vnt-cli.log 2>&1  &
 ##这是个脚本的核心
-sleep 5
+sleep 2
+
+echo  -n "$(date +%s)" > /tmp/start_timestamp_vnt_cli
 
 
 [  -z `pidof vnt-cli` ] && echo "运行失败" || echo "${vnt}  ${d} ${sz} ${n} --no-proxy & 运行成功"
